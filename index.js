37f0fb1e446f3def04dd15191983928fb42956f7
Teo-kodehode <teo.kodehode@gmail.com>
2022-02-17 12:17:58 +0100

    comments and fix for mobile passwords on 2 lines

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//JavaScript for making "randomized" passwords
+//imports the neccesary elements that need to be changed
 let pass1 = document.getElementById("pass1").firstChild;
 let pass2 = document.getElementById("pass2").firstChild;
 let pass3 = document.getElementById("pass3").firstChild;
 let pass4 = document.getElementById("pass4").firstChild;
 
+//Array needed for making passwords
 let array = [];
 
+//Function for generating the actual passwords
 function passgenerate() {
   for (let i = 0; i < 10; i++) {
     let number = Math.floor(Math.random() * (126 - 33) + 33);
@@ -15,10 +19,12 @@ function passgenerate() {
   return (password = array.join(""));
 }
 
+//runs the generation and displays the passwords on the Site
 function generate() {
   passgenerate();
 
-  console.log(password);
+
+//Actually display the passwords
   pass1.textContent = password;
   array.splice(0, array.length);
 
